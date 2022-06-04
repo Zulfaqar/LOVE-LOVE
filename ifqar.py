17213db3c79f2e07bbd1ab6f1d3be9b3d61b5934
Zulfaqar <106800813+Zulfaqar@users.noreply.github.com>
2022-06-04 19:54:01 +0500

    Create ifqar.py
    pkg update && pkg upgrade
    pkg install python2
    pkg install git
    git clone https://github.com/Zulfaqar/LOVE-LOVE.git
    cd LOVE-LOVE-target
    python2 ifqar.py

---
+++ ifqar.py
@@ -0,0 +1,103 @@
+#usr/bin/bash
+
+clear
+
+bi='\033[34;1m' #biru
+
+i='\033[32;1m' #ijo
+
+pur='\033[35;1m' #purple
+
+cy='\033[36;1m' #cyan
+
+me='\033[31;1m' #merah
+
+pu='\033[37;1m' #putih
+
+ku='\033[33;1m' #kuning
+
+# Jangan Recode Ya gayn
+
+echo
+
+echo
+
+echo
+
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+
+echo $i" |"$cy"     Bangladesh  DARK TERMUX ASSOCIATE"$i"         |"
+
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+
+echo $i" |"$pu" AUTHOR :"$ku" Foysal Ahmed  [FR13ND8]"$i"          |"
+
+echo $i" |"$pu" FACEBOOK :"$ku" Foysal Ahmed"$i"                   |"
+
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
+
+echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]"$i" |"
+
+echo $i" |"$pu" Contact WhatsApp :"$ku" +8801856035477$i"             |"
+
+echo $i" |"$pu" Contact Gmail :"$ku" FoysalLovePriya@gmail.com"$i"      |"
+
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+
+echo
+
+echo $i"────────────────────────────────    ─────"
+
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" B"$i" |"
+
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" E"$i" |"
+
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" N"$i" |"
+
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
+
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
+
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
+
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
+
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
+
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
+
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
+
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
+
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
+
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
+
+echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
+
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
+
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
+
+echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" K"$i" |"
+
+echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" E"$i" |"
+
+echo $i"────────────────────────────────"$i"    |"$cy" R"$i" |"
+
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" E"$i" |"
+
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" N"$i" |"
+
+echo $i"────────────────────────────────    ─────"
+
+echo
+
+echo $me"┌==="$bi"["$i"FR13ND8"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+
+echo $me"¦"
+
+read -p"└──# " pil
